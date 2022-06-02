72dca0a47db4940384c5fd49e9e6f5c7862380e3
Brianna Gaines <briannagaines31@gmail.com>
2022-06-01 20:16:05 -0600

    adds backend routes

--- bookmarkd-backend/server.js
+++ bookmarkd-backend/server.js
@@ -17,8 +17,54 @@ mongoose.connection
 
   const bookmarkSchema = new mongoose.Schema({
       name: String,
-      image: String,
-      title: String,
+      url: String
   })
 
   const Bookmark = mongoose.model("Bookmark", bookmarkSchema)
+
+  app.use(cors())
+  app.use(morgan("dev"))
+  app.use(express.json())
+
+app.get("/", (req,res) =>{
+    res.send ("you are home")
+})
+
+app.get("/bookmark", async (req,res)=>{
+    try{
+        res.json(await Bookmark.find({}))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+app.post("/bookmark", async (req, res) => {
+    try {
+      res.json(await Bookmark.create(req.body))
+    } catch (error) {
+      res.status(400).json(error)
+    }
+  })
+
+app.delete("/bookmark/:id", async (req, res) => {
+    try {
+      res.json(await Bookmark.findByIdAndDelete(req.params.id))
+    } catch (error) {
+      res.status(400).json(error)
+    }
+  })
+  
+  app.put("/bookmark/:id", async (req, res) => {
+    try {
+      res.json(
+        await Bookmark.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      )
+    } catch (error) {
+      res.status(400).json(error)
+    }
+  })
+
+
+
+
+app.listen(PORT, ()=> console.log(`listening on ${PORT}`))
